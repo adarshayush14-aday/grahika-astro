f488aaf0e6e788065665e49d583168e4f4c6acad
adarshayush14-aday <adarsh.ayush14@gmail.com>
2026-05-23 19:34:37 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'grahika-astro-v1';
+const urlsToCache = [
+  '/grahika-astro/',
+  '/grahika-astro/index.html',
+  '/grahika-astro/manifest.json',
+  '/grahika-astro/grahika_logo_512.png'
+];
+
+// Install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        return caches.match('/grahika-astro/index.html');
+      });
+    })
+  );
+});
