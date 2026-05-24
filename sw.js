37587b597d5ac904165f1a9741f90a849b66dc82
adarshayush14-aday <adarsh.ayush14@gmail.com>
2026-05-24 17:23:33 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,18 @@
-const CACHE_NAME = 'grahika-astro-v4';
-const urlsToCache = [
-  '/grahika-astro/',
-  '/grahika-astro/index.html',
-  '/grahika-astro/manifest.json',
-  '/grahika-astro/grahika_logo_512.png'
-];
+const CACHE_NAME = 'grahika-astro-v5';
 
-// Install
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
   self.skipWaiting();
 });
 
-// Activate
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys => 
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
   self.clients.claim();
 });
 
-// Fetch
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).catch(() => {
-        return caches.match('/grahika-astro/index.html');
-      });
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
